c9146e48dda7ba05827862b6b747c7fde226888f
Kausthub Mayuram <kausthubmayuram@yahoo.in>
2021-02-07 17:45:06 +0530

    Added changes for the version 0.0.13

--- src/lib/static-table.component.ts
+++ src/lib/static-table.component.ts
@@ -2,20 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'static-table',
-  template: `<table>
-  <thead>
-      <tr>
-          <th *ngFor="let item of columns">{{item}}</th>
-      </tr>
-  </thead>
-  <tbody>
-      <tr *ngFor="let item of data;let index = index">
-          <td *ngFor="let cell of item">{{cell}}</td>
-      </tr>
-  </tbody>
-</table>`,
-  styles: [
-  ]
+  templateUrl: './static-table.component.html',
+  styleUrls: ['./static-table.component.css']
 })
 export class StaticTableComponent implements OnInit {
 
